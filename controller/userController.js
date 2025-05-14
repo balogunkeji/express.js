1dde6ea596ffa285932d961ad12988b649120dd2
keji <tomitoyo.93@gmail.com>
2025-05-14 17:59:48 +0100

    fix: fund wallet redesign

--- controller/userController.js
+++ controller/userController.js
@@ -3,13 +3,34 @@ const getSignUp = (req,res) => {
    console.log(req.body)
 }
 
+const handleErrors = (err) => {
+    console.log(err.message, err.code);
+    let errors = { email: '', password: '' };
+
+    // duplicate email error
+    if (err.code === 11000) {
+        errors.email = 'that email is already registered';
+        return errors;
+    }
+
+    // validation errors
+    if (err.message.includes('user validation failed')) {
+        Object.values(err.errors).forEach(({ properties }) => {
+            errors[properties.path] = properties.message;
+        });
+    }
+
+    return errors;
+}
+
 const postSignUp = async (req, res) => {
     try {
         const { email, password } = req.body;
         const user = await User.create({ email, password });
         res.status(200).json({ message: "User registered!", user });
     } catch (err) {
-        res.status(500).send("Signup failed");
+        const errors = handleErrors(err);
+        res.status(400).json({ errors });
     }
 };
 
--- models/auth.js
+++ models/auth.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const { isEmail } = require('validator');
+const bcrypt = require("bcrypt");
 
 const userSchema = new Schema({
     email: {
@@ -16,6 +17,23 @@ const userSchema = new Schema({
         minlength: 8,
     }
 }, {timestamps: true});
+
+//Mongoose hooks are functions that run before or after certain actions in your model — like saving, updating, deleting, etc.
+// They are used for hashing passwords, run validations, cleanup logic, etc.
+
+//pre save: runs before the function
+userSchema.pre('save', async function(next) {
+   const salt = await bcrypt.genSalt();
+   this.password = await bcrypt.hash(this.password, salt);
+
+    next();
+})
+
+//post save: runs after the function
+userSchema.post('save', async function(doc, next) {
+    console.log('post save', doc)
+    next();
+})
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
